a7f13fa5da67ca0e2df65532e93a8820123fc9ef
randrade11 <rahif@alunos.utfpr.edu.br>
2024-12-17 23:19:23 -0300

    Update struct_card.h

--- SUPER-TRUNFO-main/Pasta do trunfo super secreto/struct_card.h
+++ SUPER-TRUNFO-main/Pasta do trunfo super secreto/struct_card.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct 
+typedef struct
 {
     char nome[15];
     char tipo;
@@ -14,44 +14,54 @@ typedef struct
     float peso;
     float altura;
     int habilidade;
-}Cards;// estrutura das cartas
+} Cards; // estrutura das cartas
 
-void SaveBin(FILE *arq_csv, FILE *arq_dat, Cards *buffer){
+void Strings(char a[], int b)
+{
+
+    setbuf(stdin, NULL);
+    fgets(a, b, stdin);
+    a[strcspn(a, "\n")] = '\0';
+}
+
+void SaveBin(FILE *arq_csv, FILE *arq_dat, Cards *buffer)
+{
 
     char header[200], linha[200];
 
-    fgets(header, 200, arq_csv);// ignora cabeçalho
-    
-    while(fgets(linha, 60, arq_csv) != NULL){
-        
-        sscanf(linha, " %14[^,], %c, %d, %d, %d, %d, %f, %f, %d",                   buffer->nome,
-                                                                                    &buffer->tipo,
-                                                                                    &buffer->numero,
-                                                                                    (int*)&buffer->trunfo,
-                                                                                    &buffer->hp,
-                                                                                    &buffer->ataque,
-                                                                                    &buffer->peso,
-                                                                                    &buffer->altura,
-                                                                                    &buffer->habilidade);
+    fgets(header, 200, arq_csv); // ignora cabeçalho
+
+    while (fgets(linha, 60, arq_csv) != NULL)
+    {
+
+        sscanf(linha, " %14[^,], %c, %d, %d, %d, %d, %f, %f, %d", buffer->nome,
+               &buffer->tipo,
+               &buffer->numero,
+               (int *)&buffer->trunfo,
+               &buffer->hp,
+               &buffer->ataque,
+               &buffer->peso,
+               &buffer->altura,
+               &buffer->habilidade);
 
         fwrite(buffer, sizeof(Cards), 1, arq_dat);
-    }// enquanto houver linhas
+    } // enquanto houver linhas
 
     rewind(arq_dat);
 
-}// salva o metagame no arquivo bin na primeira vez que você abre o jogo
+} // salva o metagame no arquivo bin na primeira vez que você abre o jogo
 
-int CountLines(FILE *arq_dat){
+int CountLines(FILE *arq_dat)
+{
 
     int lines = 0;
     long int size;
 
     fseek(arq_dat, 0, SEEK_END);
     size = ftell(arq_dat);
-    lines = size / sizeof(Cards);// verifica quantas cartas tem no arquivo bin
-    
+    lines = size / sizeof(Cards); // verifica quantas cartas tem no arquivo bin
+
     rewind(arq_dat);
 
     return lines;
-}// conta quantas linhas tem no arquivo csv (quantidade de cartas)
-
+} // conta quantas linhas tem no arquivo csv (quantidade de cartas)
