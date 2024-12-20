90676ea2d2dae3e8e49073298be0d11b9be4bdfd
saint-clg <sanctus.saint.contato@gmail.com>
2024-12-20 18:17:08 -0300

    função menu atualiada

--- SUPER-TRUNFO-main/estrutura_beta.c
+++ SUPER-TRUNFO-main/estrutura_beta.c
@@ -39,92 +39,100 @@ int main()
 
     printf("Criando deck...\n\n\n");
     fread(deck, sizeof(Cards), n_cards, arq_dat);
-
     printf("MENU\n");
-    printf("1- PLAY\n2- DECK\n3- OPTIONS\n4- EXIT\n");
-    printf("Digite sua opção:");
-    scanf("%d", &navegate);
-    printf("\n\n");
-
-    switch (navegate)
+    do
     {
-    case 1:
-        // Função Play(jogar) (em construção)
-        printf("FUNÇÃO PLAY EM CONSTRUÇÃO\n");
-        return 0;
-        break;
-
-    case 2:
-        // Abre menu de opções de configuração do deck
-        printf("1- BUSCAR\n2- FILTRAR\n3- ADICIONAR\n4- EXCLUIR\n5- ALTERAR\n6- EXPORTAR\n7- VOLTAR\n...");
-
-        // Aguardando o input do usuário para o próximo comando
-        navegate = 0;
+        /* code */
+        printf("1- PLAY\n2- DECK\n3- OPTIONS\n4- EXIT\n");
+        printf("Digite sua opção:");
         scanf("%d", &navegate);
         printf("\n\n");
 
-        // Navegação com base no comando dado
         switch (navegate)
         {
         case 1:
-            printf("🔍 ");
-            Strings(buffer.nome, sizeof(buffer.nome)); // Função para entrada de nome
-            SearchName(buffer.nome, deck, n_cards);    // Função de busca por nome
+            // Função Play(jogar) (em construção)
+            printf("FUNÇÃO PLAY EM CONSTRUÇÃO\n");
+            return 0;
             break;
 
         case 2:
-            navegate = 0; // Abertura de submenu
-            printf("FILTRAR:\n1- TIPO\n2- STATS\n3- SUPER TRUNFOS\n...");
+            
+            // Abre menu de opções de configuração do deck
+            printf("1- BUSCAR\n2- FILTRAR\n3- ADICIONAR\n4- EXCLUIR\n5- ALTERAR\n6- EXPORTAR\n7- VOLTAR\n...");
+
+            // Aguardando o input do usuário para o próximo comando
+            navegate = 0;
             scanf("%d", &navegate);
             printf("\n\n");
-            Filter(navegate, deck, arq_dat);
-            break;
+
+            // Navegação com base no comando dado
+            switch (navegate)
+            {
+            case 1:
+                printf("🔍 ");
+                Strings(buffer.nome, sizeof(buffer.nome)); // Função para entrada de nome
+                SearchName(buffer.nome, deck, n_cards);    // Função de busca por nome
+                break;
+
+            case 2:
+                navegate = 0; // Abertura de submenu
+                printf("FILTRAR:\n1- TIPO\n2- STATS\n3- SUPER TRUNFOS\n...");
+                scanf("%d", &navegate);
+                printf("\n\n");
+                Filter(navegate, deck, arq_dat);
+                break;
+
+            case 3:
+                AddCard(buffer, arq_dat);
+                n_cards = CountLines(arq_dat);
+                deck = realloc(deck, n_cards * (sizeof(Cards)));
+                fread(deck, sizeof(Cards), n_cards, arq_dat);
+                memset(&buffer, 0, sizeof(Cards));
+                break;
+
+            case 4:
+                DeletCard(deck, arq_dat);
+                n_cards = CountLines(arq_dat);
+                deck = realloc(deck, n_cards * (sizeof(Cards)));
+                rewind(arq_dat);
+                fread(deck, sizeof(Cards), n_cards, arq_dat);
+                break;
+
+            case 5:
+                SwitchStatus(deck, buffer, arq_dat);
+                memset(&buffer, 0, sizeof(Cards));
+                break;
+
+            case 6:
+                ExportCsv(arq_dat);
+                break;
+            
+            case 7:
+                break;
+
+            default:
+                printf("Opção inválida no menu de opções.\n");
+                break;
+            }
+            break; // Fim do case 2
 
         case 3:
-            AddCard(buffer, arq_dat);
-            n_cards = CountLines(arq_dat);
-            deck = realloc(deck, n_cards * (sizeof(Cards)));
-            fread(deck, sizeof(Cards), n_cards, arq_dat);
-            memset(&buffer, 0, sizeof(Cards));
+            // Options do menu principal (em construção)
+            printf("FUNÇÃO OPTIONS EM CONSTRUÇÃO\n");
             break;
-
         case 4:
-            DeletCard(deck, arq_dat);
-            n_cards = CountLines(arq_dat);
-            deck = realloc(deck, n_cards * (sizeof(Cards)));
-            rewind(arq_dat);
-            fread(deck, sizeof(Cards), n_cards, arq_dat);
-            break;
-
-        case 5:
-            SwitchStatus(deck, buffer, arq_dat);
-            memset(&buffer, 0, sizeof(Cards));
-            break;
-
-        case 6:
-            ExportCsv(arq_dat);
+            printf("Saindo do arquivo rsrsrs\n");
             break;
 
         default:
-            printf("Opção inválida no menu de opções.\n");
+            printf("OPÇÃO INVALIDA NO MENU DE OPÇOES BURRO!\n");
             break;
         }
-        break; // Fim do case 2
 
-    case 3:
-        // Options do menu principal (em construção)
-        printf("FUNÇÃO OPTIONS EM CONSTRUÇÃO\n");
-        break;
+    } while (navegate != 4);
+    
 
-    case 4:
-        // Exit do menu principal (em construção)
-        printf("FUNÇÃO EXIT EM CONSTRUÇÃO\n");
-        break;
-
-    default:
-        printf("DEFAULT!\n");
-        break;
-    }
 
     fclose(arq_dat);
     free(deck);
